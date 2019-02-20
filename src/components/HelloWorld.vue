36a4a0720666cbcad508628b35d709980ce02be5
guohao <511721582@qq.com>
2019-02-20 10:00:44 +0800

    vue前端项目构架

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-main>
+          <h2>活动域</h2>
+          <h3><a @click="goDemo">点击跳转</a></h3>
+          <h3><a @click="getData">调用接口</a></h3>
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    goDemo () {
+      this.$router.push('demo')
+    },
+    getData () {
+      this.$axios.get('/api/ms-fahuobao-user/content/findContentInfoByTypeNoNotToken', {typeNo: 'FHBContentType0101'}).then(response => {
+        console.log(response, 'response')
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+</style>
